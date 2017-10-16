07d0bcad75df92bd8b81091e02f424114f1fe2cb
Rimjhim Bhadani <rimjhimbhadani@gmail.com>
2017-10-16 12:22:55 +0530

    Update StartActivity.java

--- app/src/main/java/com/example/android/makecookies/StartActivity.java
+++ app/src/main/java/com/example/android/makecookies/StartActivity.java
@@ -3,11 +3,20 @@ package com.example.android.makecookies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+Button startButton;
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+         startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
